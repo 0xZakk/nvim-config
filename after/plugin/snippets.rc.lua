f0a6d62ffa08527decff53dd43aa1122e7bf643f
Zakk <zakk@hawthorne.io>
2022-11-02 07:19:42 -0500

    implement markdown snippets for posts, notes, references, etc

---
+++ after/plugin/snippets.rc.lua
@@ -0,0 +1,4 @@
+require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
+
+require('luasnip').filetype_extend("html", { "liquid" })
