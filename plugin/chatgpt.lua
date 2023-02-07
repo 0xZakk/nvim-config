6cc9c59b81a38df8a4b7ef9fe4cdc0fd2b399a49
Zakk <zakk@hawthorne.io>
2023-02-07 09:50:28 -0500

    update default chatgpt settings

--- plugin/chatgpt.lua
+++ plugin/chatgpt.lua
@@ -2,8 +2,14 @@
 local chatgpt = require('chatgpt')
 
 chatgpt.setup({
-  welcome_message = 'Welcome to ChatGPT.nvim!',
-  max_line_length = 250,
+  welcome_message = 'Welcome to ChatGPT.nvim',
+  max_line_length = 500,
 })
 
+-- A keymap to open the basic chat window
 vim.keymap.set('n', '<Leader>ee', ':ChatGPT<CR>', { noremap = true, desc = 'ChatGPT' })
+-- A keymap to open the Act As chat window
+vim.keymap.set('n', '<Leader>ea', ':ChatGPTActAs<CR>', { noremap = true, desc = 'ChatGPTActAs' })
+-- A keymap to open the instruction-based chat window
+vim.keymap.set('n', '<Leader>ep', ':ChatGPTEditWithInstructions<CR>',
+  { noremap = true, desc = 'ChatGPTEditWithInstructions' })
