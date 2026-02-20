d2ad1d3bb7f33da2c00a4d79cc264bb25ebf93b3
0xZakk <zakk@hawthorne.io>
2026-02-20 11:32:39 -0600

    fix: update treesitter config for new nvim-treesitter API
    The nvim-treesitter.configs module was removed in newer versions.
    Use lazy.nvim's main/opts pattern instead of manual require.
    Remove deprecated autotag option and tsx parser patch.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,35 +3,27 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPre", "BufNewFile" },
-  build = function()
-    pcall(require("nvim-treesitter.install").update { with_sync = true })
-  end,
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      highlight = { enable = true },
-      indent = { enable = true },
-      autotag = { enable = true },
-      ensure_installed = {
-        "tsx",
-        "json",
-        "yaml",
-        "css",
-        "html",
-        "lua",
-        "bash",
-        "graphql",
-        "markdown",
-        "prisma",
-        "javascript",
-        "python",
-        "scss",
-        "solidity",
-        "sql",
-      },
-    })
-
-    -- Patch parser config for tsx
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
-  end,
+  build = ":TSUpdate",
+  main = "nvim-treesitter.configs",
+  opts = {
+    highlight = { enable = true },
+    indent = { enable = true },
+    ensure_installed = {
+      "tsx",
+      "json",
+      "yaml",
+      "css",
+      "html",
+      "lua",
+      "bash",
+      "graphql",
+      "markdown",
+      "prisma",
+      "javascript",
+      "python",
+      "scss",
+      "solidity",
+      "sql",
+    },
+  },
 }
